95d5103d2df165a1914f4ef405076940b8351510
Rohit Kumar <ctiviono@localhost.localdomain>
2022-07-15 23:35:59 +0530

    first commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const io = require('socket.io')(3000)
 const users = {}
 
 io.on('connection', socket => {
-  socket.on( 'new-user', name =>{
+  socket.on( 'new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
   })
-  socket.on('send-chat-input', message=>{
+  socket.on('send-chat-message', message=>{
     socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
   })
   socket.on( 'disconnect', () =>{
